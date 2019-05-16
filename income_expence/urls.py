39f11da9b5b2deca2e014af348946a1fd421a21f
YaGiNA <yutachihaya@gmail.com>
2019-05-16 18:00:06 +0900

    Fix typo

--- income_expence/urls.py
+++ income_expence/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('expence', include('expence.urls')),
+    path('expense', include('expense.urls')),
     path('admin/', admin.site.urls),
 ]
