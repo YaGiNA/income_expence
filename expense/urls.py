e0bb7910eb3921e2bb8e50f33dc249e30357790a
YaGiNA <yutachihaya@gmail.com>
2019-05-20 21:16:33 +0900

    Add template of list & detail

--- expense/urls.py
+++ expense/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = 'expense'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:expense_id>/', views.detail, name='detail'),
 ]
--- expense/views.py
+++ expense/views.py
@@ -1,8 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 # Create your views here.
-from django.http import HttpResponse
+from .models import Expense
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_expense_list = Expense.objects.order_by('-pub_date')[:5]
+    context = {
+        'latest_expense_list': latest_expense_list,
+    }
+    return render(request, 'expense/index.html', context)
+
+def detail(request, expense_id):
+    expense = get_object_or_404(Expense, pk=expense_id)
+    return render(request, 'expense/detail.html', {'expense': expense})
