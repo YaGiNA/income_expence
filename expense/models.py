b38039fdf5ecc69c56407a8bea9c35be01cb4890
YaGiNA <yutachihaya@gmail.com>
2019-05-20 20:57:21 +0900

    Make method of data model

--- expense/models.py
+++ expense/models.py
@@ -1,7 +1,15 @@
 from django.db import models
+from django.utils import timezone
+import datetime
 
 # Create your models here.
 class Expense(models.Model):
     title = models.CharField(max_length=200)
     price = models.IntegerField()
     pub_date = models.DateTimeField('date of paid')
+
+    def __str__(self):
+        return self.title
+
+    def was_paid_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
