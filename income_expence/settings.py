b7c0a3c042492f50e8e8adcc6e4ef0db210a6322
YaGiNA <yutachihaya@gmail.com>
2019-05-21 02:29:25 +0900

    Removed key

--- income_expence/settings.py
+++ income_expence/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'h0$d+c(96c4xal63a=qlmu&p3lwdhw147@bi3r3hd(-ng#e66u'
-
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
